deea5c65ea18374dbf365f481eea3f145222217e
qdspr <queuedespair@protonmail.com>
2022-01-27 12:14:57 +0000

    fix: automatically refresh last scanned block

--- ui/src/hooks/util/useGetLastScannedBlock.tsx
+++ ui/src/hooks/util/useGetLastScannedBlock.tsx
@@ -1,9 +1,17 @@
 import axios from "axios";
+import { useEffect } from "react";
+import { useBlockNumber } from "wagmi";
 import { serverUrl } from "../../utils/env";
 import { LoadingOrErroredValue, useAsyncValue } from "./useAsyncValue";
 
 export function useGetLastScannedBlock(): [number | null, boolean, LoadingOrErroredValue['forceRefresh']] {
+  const [{data: blockNumber}] = useBlockNumber();
   const val = useAsyncValue(() => axios.get(`${serverUrl}/jewel/last-scanned-block`));
+
+  useEffect(() => {
+    if(val.loading) return;
+    val.refreshWithoutLoading();
+  }, [blockNumber])
 
   if (val.value?.data && typeof val.value?.data === 'number') {
     return [val.value.data, !!val.loading, val.forceRefresh];
