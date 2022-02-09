22e339b917c7b7e4b538622984f60abdb3c900d8
0xcamel <0xcamel@protonmail.com>
2022-02-09 20:20:25 +0000

    feat: use custom gaslimit

--- ui/src/hooks/utxo/useRedeemFromUTXO.ts
+++ ui/src/hooks/utxo/useRedeemFromUTXO.ts
@@ -16,10 +16,15 @@ export function useRedeemFromUTXO(
     "fullRedeem"
   );
 
+  console.log("fullRedeem", utxoAddress, jewelAmount.toString());
+
   const fn = useCallback(async () => {
     try {
       setLoading(true);
-      const output = await write({ args: [utxoAddress, jewelAmount] });
+      const output = await write({
+        args: [utxoAddress, jewelAmount.toString()],
+        overrides: { gasLimit: BigNumber.from("500000") },
+      });
       if (output.error) throw output.error;
       await output.data?.wait(2);
       setLoading(false);
