01e622f7aa375f45ff852b650444c4198557929b
0xcamel <0xcamel@protonmail.com>
2022-02-18 01:16:50 +0000

    feat: work on swap hooks

--- ui/src/components/SwapAsset.tsx
+++ ui/src/components/SwapAsset.tsx
@@ -3,12 +3,16 @@ import NumericInput from "./NumericInput";
 
 interface SwapAsset {
   onChange(x?: string): void;
+  value?: string;
 }
 
-const InputPanel: React.FC<Pick<SwapAsset, "onChange">> = ({ onChange }) => {
+const InputPanel: React.FC<Pick<SwapAsset, "onChange" | "value">> = ({
+  onChange,
+  value,
+}) => {
   return (
     <div className="h3 relative flex items-baseline flex-grow gap-3 overflow-hidden">
-      <NumericInput value={69} onUserInput={onChange} />
+      <NumericInput value={value || ""} onUserInput={onChange} />
     </div>
   );
 };
