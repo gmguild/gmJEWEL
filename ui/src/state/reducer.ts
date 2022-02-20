e381dfb7a830c46ee5d55ee6ac337ebd2d3d8fd0
0xcamel <0xcamel@protonmail.com>
2022-02-20 12:31:49 +0000

    feat: token lists

--- ui/src/state/reducer.ts
+++ ui/src/state/reducer.ts
@@ -3,10 +3,12 @@ import { combineReducers } from "@reduxjs/toolkit";
 import multicall from "./multicall/reducer";
 import swap from "./swap/reducer";
 import application from "./application/reducer";
+import lists from "./lists/reducer";
 
 const reducer = combineReducers({
   multicall,
   swap,
+  lists,
   application,
 });
 
