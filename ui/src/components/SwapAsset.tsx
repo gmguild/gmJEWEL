45c6f17f263a7f19db0636a92d53dfaad49293f1
0xcamel <0xcamel@protonmail.com>
2022-02-20 17:42:37 +0000

    feat: start to add currency selector

--- ui/src/components/SwapAsset.tsx
+++ ui/src/components/SwapAsset.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { Currency } from "../package";
 import NumericInput from "./NumericInput";
 
 interface SwapAsset {
   onChange(x?: string): void;
   value?: string;
+  onSelect?(x: Currency): void;
 }
 
 const InputPanel: React.FC<Pick<SwapAsset, "onChange" | "value">> = ({
@@ -17,10 +19,16 @@ const InputPanel: React.FC<Pick<SwapAsset, "onChange" | "value">> = ({
   );
 };
 
-export const SwapAsset = ({ onChange }: SwapAsset) => {
+const InputPanelHeader: React.FC<Pick<SwapAsset, "onSelect">> = ({
+  onSelect,
+}) => {
+  return <div>Header</div>;
+};
+
+export const SwapAsset = ({ onChange, onSelect }: SwapAsset) => {
   return (
     <div className="rounded-[14px] border border-dark-700 hover:border-dark-600 bg-dark-900 p-3 flex flex-col gap-4">
-      Header
+      <InputPanelHeader onSelect={onSelect} />
       <div className="flex gap-1 justify-between items-baseline px-1.5">
         <InputPanel onChange={onChange} /> Balance
       </div>
