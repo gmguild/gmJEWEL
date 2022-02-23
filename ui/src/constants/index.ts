b3814026909ec3d375516a51888841525ec4cfaa
0xcamel <0xcamel@protonmail.com>
2022-02-23 01:26:46 +0000

    feat: nearly have trade

--- ui/src/constants/index.ts
+++ ui/src/constants/index.ts
@@ -9,4 +9,33 @@ export const BETTER_TRADE_LESS_HOPS_THRESHOLD = new Percent(
   JSBI.BigInt(10000)
 );
 
+// one basis point
+export const ONE_BIPS = new Percent(JSBI.BigInt(1), JSBI.BigInt(10000));
+export const BIPS_BASE = JSBI.BigInt(10000);
+
 export const DEFAULT_DEADLINE_FROM_NOW = 60 * 30;
+// used for warning states
+export const ALLOWED_PRICE_IMPACT_LOW: Percent = new Percent(
+  JSBI.BigInt(100),
+  BIPS_BASE
+); // 1%
+export const ALLOWED_PRICE_IMPACT_MEDIUM: Percent = new Percent(
+  JSBI.BigInt(300),
+  BIPS_BASE
+); // 3%
+export const ALLOWED_PRICE_IMPACT_HIGH: Percent = new Percent(
+  JSBI.BigInt(500),
+  BIPS_BASE
+); // 5%
+
+// if the price slippage exceeds this number, force the user to type 'confirm' to execute
+export const PRICE_IMPACT_WITHOUT_FEE_CONFIRM_MIN: Percent = new Percent(
+  JSBI.BigInt(1000),
+  BIPS_BASE
+); // 10%
+
+// for non expert mode disable swaps above this
+export const BLOCKED_PRICE_IMPACT_NON_EXPERT: Percent = new Percent(
+  JSBI.BigInt(1500),
+  BIPS_BASE
+); // 15%
