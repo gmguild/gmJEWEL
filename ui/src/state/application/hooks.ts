a121bfb3cb706d818fcb571ca721c5be678aac39
0xcamel <0xcamel@protonmail.com>
2022-02-25 00:08:00 +0000

    feat: web3connect

--- ui/src/state/application/hooks.ts
+++ ui/src/state/application/hooks.ts
@@ -50,6 +50,38 @@ export function useCloseModals(): () => void {
   return useCallback(() => dispatch(setOpenModal(null)), [null]);
 }
 
+export function useWalletModalToggle(): () => void {
+  return useToggleModal(ApplicationModal.WALLET);
+}
+
+export function useNetworkModalToggle(): () => void {
+  return useToggleModal(ApplicationModal.NETWORK);
+}
+
+export function useToggleSettingsMenu(): () => void {
+  return useToggleModal(ApplicationModal.SETTINGS);
+}
+
+export function useShowClaimPopup(): boolean {
+  return useModalOpen(ApplicationModal.CLAIM_POPUP);
+}
+
+export function useToggleShowClaimPopup(): () => void {
+  return useToggleModal(ApplicationModal.CLAIM_POPUP);
+}
+
+export function useToggleSelfClaimModal(): () => void {
+  return useToggleModal(ApplicationModal.SELF_CLAIM);
+}
+
+export function useToggleDelegateModal(): () => void {
+  return useToggleModal(ApplicationModal.DELEGATE);
+}
+
+export function useToggleVoteModal(): () => void {
+  return useToggleModal(ApplicationModal.VOTE);
+}
+
 export function useAddPopup(): (content: PopupContent, key?: string) => void {
   const dispatch = useAppDispatch();
   return useCallback(
