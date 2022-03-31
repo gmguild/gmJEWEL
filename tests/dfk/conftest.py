6e125a699671794b5eafc7916d798b4f89a695d5
0xcamel <0xcamel@protonmail.com>
2022-03-31 19:55:46 +0100

    feat: remove unneccesary tests

--- tests/dfk/conftest.py
+++ tests/dfk/conftest.py
@@ -48,57 +48,6 @@ def central_bank(deployer, crystal_token, gm_crystal, CentralBank):
     yield contract
 
 
-@pytest.fixture(scope="module")
-def gmg_token(deployer, GMGToken):
-    yield deployer.deploy(GMGToken)
-
-
-@pytest.fixture(scope="module")
-def staked_gmg(deployer, StakedGMG, gmg_token, crystal_token):
-    yield deployer.deploy(StakedGMG, gmg_token, deployer, uniswap_router, crystal_token)
-
-
-@pytest.fixture(scope="module")
-def uniswap_pool1(deployer, gm_crystal, crystal_token, interface):
-    contract = interface.IUniswapV2Factory(uniswap_factory)
-    contract.createPair(gm_crystal, crystal_token, {"from": deployer})
-    pool_addr = contract.getPair(gm_crystal, crystal_token)
-    pool = interface.IUniswapV2Pair(pool_addr)
-    crystal_token.transfer(
-        pool, 5_000_000 * 1e18, {"from": "0xa9ce83507d872c5e1273e745abcfda849daa654f"}
-    )
-    gm_crystal.mint(pool, 10_000_000 * 1e18, {"from": deployer})
-    pool.mint(deployer, {"from": deployer})
-    yield contract
-
-
-@pytest.fixture(scope="module")
-def uniswap_pool2(deployer, gmg_token, crystal_token):
-    contract = interface.IUniswapV2Factory(uniswap_factory)
-    contract.createPair(gmg_token, crystal_token, {"from": deployer})
-    pool_addr = contract.getPair(gmg_token, crystal_token)
-    pool = interface.IUniswapV2Pair(pool_addr)
-    crystal_token.transfer(
-        pool, 5_000_000 * 1e18, {"from": "0xa9ce83507d872c5e1273e745abcfda849daa654f"}
-    )
-    gmg_token.mint(pool, 10_000_000 * 1e18, {"from": deployer})
-    pool.mint(deployer, {"from": deployer})
-    yield contract
-
-
-@pytest.fixture(scope="module")
-def master_jeweler(deployer, MasterJeweler, gmg_token):
-    yield deployer.deploy(
-        MasterJeweler,
-        gmg_token,
-        deployer,
-        1 * 1e17,
-        20_000_000,
-        [20_000_000, 25_000_000, 30_000_000, 35_000_000, 45_000_000],
-        [100, 50, 30, 10, 1],
-    )
-
-
 # DefiKingdom contracts
 
 
